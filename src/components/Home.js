ee715f7e1d11ca067f900ec21f3e3cdb90bd5fc3
Khushi Balodi <khushibalodi@gmail.com>
2021-05-01 00:42:13 +0530

    homepage completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,27 @@ import logo from "./logoapp.png";
 
 const Home=()=>{
     return (
+        <>
         <div className="home">
             <img src={logo} alt="logo" className="mt-4 logoimg"/>
+            <div id="homepage-text">Finding missing pets and more..</div>
         </div>
+         <div>
+         <ul className="box-area">
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+         </ul>
+       </div>
+       </>
     )
 }
 
