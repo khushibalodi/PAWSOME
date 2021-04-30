465bef1f87bcb5ec5e157b6449b2f3ee24f89e94
Khushi Balodi <khushibalodi@gmail.com>
2021-04-30 21:13:49 +0530

    added logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from "./logoapp.png";
 
 const Home=()=>{
     return (
         <div className="home">
+            <img src={logo} alt="logo" className="mt-4 logoimg"/>
         </div>
     )
 }
